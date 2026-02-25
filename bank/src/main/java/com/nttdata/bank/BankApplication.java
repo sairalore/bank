648cea9f4514e1d27fd0cadebf681ddae62ade19
sairalore <sairalore@hotmail.com>
2026-02-25 09:39:06 -0500

    Initial commit

---
+++ bank/src/main/java/com/nttdata/bank/BankApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
